2eabbf37d2630606160d249b80346a8baa078b32
Huy-Ngo <duchuy29092000@gmail.com>
2020-05-07 12:33:59 +0700

    Rename models and add methods for class

--- models/posts.py
+++ models/posts.py
@@ -1,8 +1,27 @@
-from .db import db
+from models.db import db
 
 
-class Post(db.Model):
+class PostModel(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(120))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def json(self):
+        return {'id': self.id, 'content': self.content}
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- models/users.py
+++ models/users.py
@@ -1,9 +1,31 @@
-from .db import db
+from models.db import db
 
 
-class User(db.Model):
+class UserModel(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
 
     posts = db.relationship('Post', backref='user', lazy=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {'id': self.id, 'name': self.name}
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
